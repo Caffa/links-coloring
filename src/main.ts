f32d0bc778281556367e2051ae095671424fbf61
caffae <wangyeelin@gmail.com>
2026-02-22 00:49:14 +0800

    doing a bug fix for the linter and Obsidian UI guidelines

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ export default class LinkColorPlugin extends Plugin {
     }
 
     async loadSettings() {
-        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData()) as LinkColorSettings;
     }
 
     async saveSettings() {
